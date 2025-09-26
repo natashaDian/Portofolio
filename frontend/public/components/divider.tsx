592f7a2f9223ba2701b6613353905af5a13e16bb
natashaDian <natashadiann.m@gmail.com>
2025-09-26 11:15:32 +0700

    finish layout tahap 2

---
+++ frontend/public/components/divider.tsx
@@ -0,0 +1,12 @@
+export default function divider(){
+    return(
+    <div className="flex items-center w-full">
+        <span className="h-px flex-1 bg-pink-500 shadow-[0_0_20px_#ff00ff,0_0_20px_#ff00ff]"></span>
+
+        <span className="shrink-0 px-4 text-pink-400 font-semibold drop-shadow-[0_0_6px_#ff00ff] text-4xl"> Project </span>
+
+        <span className="h-px flex-1 bg-pink-500 shadow-[0_0_20px_#ff00ff,0_0_20px_#ff00ff]"></span>
+    </div>
+
+)};
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SideNavbar from "../../public/components/sideNavbar";
+import Divider  from "../../public/components/divider";
 
 export default function Home() {
   return (
@@ -11,14 +12,31 @@ export default function Home() {
       <main className="flex flex-col ml-24 space-y-4 my-4 w-full  ">
         <div className="flex flex-row  min-h-screen items-center justify-between">
           <div className="flex flex-col flex-1 w-170 min-h-screen justify-center items-start">
-            <h2 className="mx-24 text-white">Data Engineer | Software Engineer</h2>
-            <h1 className="mx-24 text-4xl text-pink-700 text-shadow-pink-300">Natasha Dian Mahardita</h1>
-            <h2 className="mx-24 text-white mt-2 pt-2">An undergraduate Computer Science student passionate about building data-driven solutions and software applications. Interested in building efficient systems and solving real-world problems through technology. Let's connect with me!</h2>
+            <h2 className="mx-24 my-4 text-white rounded-full">Data Engineer | Software Engineer</h2>
+            <h1 className="mx-24 font-semibold text-4xl text-pink-300 drop-shadow-[0_0_10px_#ff00ff]">Natasha Dian Mahardita</h1>
+            <h2 className="mx-24 my-4 text-white pt-2">An undergraduate Computer Science student passionate about building data-driven solutions and software applications. Interested in building efficient systems and solving real-world problems through technology. Let&apos;s connect with me!</h2>
           </div>
           <div className="flex flex-1 bg-amber-50 w-170 min-h-screen justify-start items-center">
           </div>
         </div>
-        <div className="flex min-h-screen items-center justify-center bg-amber-600"></div>
+        <div className="flex flex-col min-h-screen space-x-4 items-start justify-center w-full">
+          <Divider />
+          <div className="flex flex-row space-x-8 p-6">
+            {["Data", "Software"].map((label, i) => (
+              <div
+                key={i}
+                className="relative rounded-full w-24 h-12 bg-pink-500 flex items-center justify-center"
+              >
+                {/* Layer glow */}
+                <div className="absolute inset-0 rounded-full shadow-[0_0_10px_#ff00ff,0_0_15px_#ff00ff]"></div>
+                <span className="relative z-10 text-white font-medium">
+                  {label}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className=""></div>
+        </div>
         <div className="flex min-h-screen items-center justify-center bg-amber-600"></div>
       </main>
     </div>
